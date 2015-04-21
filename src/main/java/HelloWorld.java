bbcc5b5043827e35cf3fa6d3a659470d448f921e
tmcbride <tmcbride@troymcbride.com>
2015-04-21 10:05:20 -0400

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by tmcbride on 4/21/2015.
+ */
+public class HelloWorld {
+    HelloWorld() {
+        System.out.print("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld w = new HelloWorld();
+    }
+
+}
